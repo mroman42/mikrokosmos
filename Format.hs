f130d4da87c41e5fb225d2088f51d93fa522d2d8
M42 <mromang08@gmail.com>
2016-09-02 13:58:15 +0200

    Name format

--- Format.hs
+++ Format.hs
@@ -5,6 +5,9 @@ import System.Console.ANSI
 promptColor :: Color
 promptColor = Blue
 
+nameColor :: Color
+nameColor = Green
+
 formatFormula :: String
 formatFormula = setSGRCode [SetColor Foreground Dull promptColor]
 
@@ -13,6 +16,9 @@ formatIntro = setSGRCode [SetConsoleIntensity BoldIntensity, SetColor Foreground
 
 formatPrompt :: String
 formatPrompt = setSGRCode [SetConsoleIntensity BoldIntensity, SetColor Foreground Vivid promptColor]
+
+formatName :: String
+formatName = setSGRCode [SetColor Foreground Dull nameColor]
 
 end :: String
 end  = setSGRCode []
