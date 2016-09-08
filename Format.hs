56f046ce33de17053d8f31bda14b898485dcf846
M42 <mromang08@gmail.com>
2016-09-09 00:45:22 +0200

    Colors Lambda reductions

--- Format.hs
+++ Format.hs
@@ -8,8 +8,11 @@ promptColor = Blue
 nameColor :: Color
 nameColor = Green
 
+substColor :: Color
+substColor = Red
+
 formatFormula :: String
-formatFormula = setSGRCode [SetColor Foreground Dull promptColor]
+formatFormula = setSGRCode [SetConsoleIntensity NormalIntensity, SetColor Foreground Dull promptColor]
 
 formatIntro :: String
 formatIntro = setSGRCode [SetConsoleIntensity BoldIntensity, SetColor Foreground Dull promptColor]
@@ -19,6 +22,12 @@ formatPrompt = setSGRCode [SetConsoleIntensity BoldIntensity, SetColor Foregroun
 
 formatName :: String
 formatName = setSGRCode [SetColor Foreground Dull nameColor]
+
+formatSubs1 :: String
+formatSubs1 = setSGRCode [SetConsoleIntensity BoldIntensity, SetColor Foreground Dull substColor]
+
+formatSubs2 :: String
+formatSubs2 = setSGRCode [SetColor Foreground Dull Yellow]
 
 end :: String
 end  = setSGRCode []
