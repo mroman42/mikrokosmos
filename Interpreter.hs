2ecdf0656872ba03f8124f983538112218d55c84
M42 <mromang08@gmail.com>
2016-10-02 19:26:45 +0200

    Interpreter environment

--- Interpreter.hs
+++ Interpreter.hs
@@ -7,21 +7,9 @@ This module contains auxiliary logic, types and representations of
 the internal state of the interpreter.
 -}
 module Interpreter
-  (
-  -- * Evaluation contexts
-    Context
-  , emptyContext
-  
-  -- * Interpreter and environment options
-  , InterpreterOptions (InterpreterOptions)
-  , defaultOptions
-  , changeVerbose
-  , changeColor
-  , getVerbose
-  , getColor
-
+  (  
   -- * Interpreter actions
-  , InterpreterAction (..)
+    InterpreterAction (..)
   , interpreteractionParser
   , act
   , multipleAct
@@ -34,53 +22,10 @@ import           Control.Applicative           ((<$>), (<*>))
 import           Control.Monad.State.Lazy      
 import           Text.ParserCombinators.Parsec hiding (State)
 import           Data.Char
-import           Data.List
-import           MultiBimap
-import           NamedLambda
 import           Format
+import           Environment
+import           NamedLambda
 import           Lambda
-
--- | A context is an application between expressions and the names
--- they may have.
-type Context  = MultiBimap Exp String
-
--- | Empty context without any bindings
-emptyContext :: Context
-emptyContext = MultiBimap.empty
-
-
--- Interpreter options
--- | Configuration options for the interpreter. They can be changed dinamically.
-data InterpreterOptions = InterpreterOptions
-  { verbose :: Bool -- ^ true to produce verbose output
-  , color :: Bool   -- ^ true to color the output
-  }
-
--- | Default configuration options for the interpreter.
-defaultOptions :: InterpreterOptions
-defaultOptions = InterpreterOptions
-  { verbose = False
-  , color   = True
-  }
-
--- | Gets the verbose configuration
-getVerbose :: InterpreterOptions -> Bool
-getVerbose = verbose
-
--- | Gets the color configuration
-getColor :: InterpreterOptions -> Bool
-getColor = color
-
--- | Sets the verbose configuration on/off.
-changeVerbose :: InterpreterOptions -> InterpreterOptions
-changeVerbose options = options {verbose = not $ verbose options}
-
--- | Sets the color configuration on/off
-changeColor :: InterpreterOptions -> InterpreterOptions
-changeColor options = options {color = not $ color options}
-
-
-
 
 
 -- | Interpreter action. It can be a language action (binding and evaluation)
@@ -104,42 +49,36 @@ data Action = Bind (String, NamedLambda)     -- ^ bind a name to an expression
 
 -- | Executes a language action. Given a context and an action, returns
 -- the new context after the action and a text output.
-act :: Action -> State Context [String]
+act :: Action -> State Environment [String]
 act Comment = return [""]
 act (Bind (s,le)) =
-  do modify (\ctx -> MultiBimap.insert (toBruijn ctx le) s ctx)
+  do modify (\env -> addBind env s (toBruijn (context env) le))
      return [""]
 act (EvalBind (s,le)) =
-  do modify (\ctx -> MultiBimap.insert (simplifyAll $ toBruijn ctx le) s ctx)
+  do modify (\env -> addBind env s (simplifyAll $ toBruijn (context env) le))
      return [""]
 act (Execute le) =
-  do context <- get
+  do env <- get
      return [unlines $
-             [ show le ] ++
-             [ unlines $ map showReduction $ simplifySteps $ toBruijn context le ] ++
-             [ showCompleteExp context $ simplifyAll $ toBruijn context le ]
+              [ show le ] ++
+              [ unlines $ map showReduction $ simplifySteps $ toBruijn (context env) le ] ++
+              [ showCompleteExp env $ simplifyAll $ toBruijn (context env) le ]
             ]
 
 
 -- | Executes multiple actions. Given a context and a set of actions, returns
 -- the new context after the sequence of actions and a text output.
-multipleAct :: [Action] -> State Context [String]
+multipleAct :: [Action] -> State Environment [String]
 multipleAct actions = concat <$> mapM act actions
 
 
 -- | Shows an expression and the name that is bound to the expression
 -- in the current context
-showCompleteExp :: Context -> Exp -> String
-showCompleteExp context expr = case getExpressionName context expr of
+showCompleteExp :: Environment -> Exp -> String
+showCompleteExp environment expr = case getExpressionName environment expr of
   Nothing      -> show (nameExp expr)
   Just expName -> show (nameExp expr) ++ formatName ++ " ⇒ " ++ expName ++ end
 
-
--- | Given an expression, returns its name if it is bounded to any.
-getExpressionName :: Context -> Exp -> Maybe String
-getExpressionName context expr = case MultiBimap.lookup expr context of
-  [] -> Nothing
-  xs -> Just $ intercalate ", " xs
 
 
 
