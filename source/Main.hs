695d9d1bdd220c33402b32f366617f6be55d4a13
M42 <mromang08@gmail.com>
2017-08-02 13:22:19 +0200

    Main function

--- source/Main.hs
+++ source/Main.hs
@@ -141,16 +141,10 @@ outputActions environment output = do
     mapM_ (outputStr . format) output
     outputStr end
   where
-    format = formatColor -- . formatVerbose
-
+    format = formatColor
     formatColor s
       | getColor environment = s
       | otherwise            = unlines $ map decolor $ lines s
-    
-    -- formatVerbose "" = ""
-    -- formatVerbose s
-    --   | not (getVerbose environment) = (++"\n") . last . lines $ s
-    --   | otherwise                    = s
 
 
 
