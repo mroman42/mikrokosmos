f8ff0fb52e6fe7fde7f6d3709e8a5a3ad35d80ef
M42 <mromang08@gmail.com>
2017-09-09 10:20:02 +0200

    Block module

--- source/Gentzen.hs
+++ source/Gentzen.hs
@@ -7,89 +7,29 @@ where
 import Types
 import Lambda
 import NamedLambda
+import Block
 import Data.Bifunctor
 import qualified Data.Map as Map
 
-type Matrix a = [[a]]
-type CharMatrix = Matrix Char
-
-height :: Matrix a -> Int
-height = length
-width :: [[a]] -> Int
-width []     = 0
-width (x:_) = length x
-
-joinMatrices :: a -> Matrix a -> Matrix a -> Matrix a
-joinMatrices space u v = zipWith (++) us vs
-  where
-    maxheight = max (height u) (height v)
-    us = replicate (maxheight - height u) (replicate (width u) space) ++ u
-    vs = replicate (maxheight - height v) (replicate (width v) space) ++ v
-
-(<::>) :: CharMatrix -> CharMatrix -> CharMatrix
-(<::>) = joinMatrices ' '
-
-hsepChar :: Char
-hsepChar = '─'
-
-stackMatrices :: String -> CharMatrix -> CharMatrix -> CharMatrix
-stackMatrices label u v = ut ++ [hline] ++ vt
-  where
-    mw = max (width u) (width v)
-    us = centerMatrix mw u
-    vs = centerMatrix mw v
-    hline = replicate mw hsepChar ++ label
-    ut = us <::> [replicate (length label) ' ']
-    vt = vs <::> [replicate (length label) ' ']
-
-
-centerMatrix :: Int -> CharMatrix -> CharMatrix
-centerMatrix n = map (centerRow n)
-
-centerRow :: Int -> String -> String
-centerRow n s = centered
-  where
-    w = length s
-    diff = n - w
-    semicentered = replicate (diff `div` 2 + diff `mod` 2) ' ' ++ s
-    centered = take n (semicentered ++ repeat ' ')
-
-showMatrixChar :: CharMatrix -> String
-showMatrixChar = unlines
-
-textMatrix :: String -> CharMatrix
-textMatrix s = [centerRow (length s) s]
-
-deductionMatrix :: CharMatrix -> String -> [CharMatrix] -> CharMatrix
-deductionMatrix inference label blocks = stackMatrices label top inference
-  where
-    top =
-      if not (null blocks)
-        then foldr1 (\x y -> x <::> ["   "] <::> y) blocks
-        else [""]
 
 data ProofTree a l = Inference a | Deduction a l [ProofTree a l]
+
 instance Bifunctor ProofTree where
   bimap f _ (Inference a)      = Inference (f a)
   bimap f g (Deduction a l ps) = Deduction (f a) (g l) (map (bimap f g) ps)
 
-matrixProofTree :: ProofTree String String -> CharMatrix
-matrixProofTree (Inference x)      = textMatrix x
-matrixProofTree (Deduction x l xs) = deductionMatrix (textMatrix x) l (map matrixProofTree xs)
+proofBlock :: ProofTree String String -> Block
+proofBlock (Inference x)      = textBlock x
+proofBlock (Deduction x l xs) = deductionBlock (textBlock x) l (map proofBlock xs)
 
-boxAround :: CharMatrix -> CharMatrix
-boxAround cm =
-  ["╭" ++ replicate w '─' ++ "╮"] ++
-  map (" "++) cm ++
-  ["╰" ++ replicate w '─' ++ "╯"]
-  where
-    w = width cm
-
+-- | Draws a complete derivation tree
 showProofTree :: ProofTree String String -> String
-showProofTree = showMatrixChar . boxAround . matrixProofTree
+showProofTree = show . box . proofBlock
 
 
-data Label = Lponens | Labs | Lpair | Lpi1 | Lpi2 | Linr | Linl | Lcase | Lunit | Labort | Labsurd
+data Label = Lponens | Labs | Lpair | Lpi1 | Lpi2
+           | Linr | Linl | Lcase | Lunit | Labort | Labsurd
+           
 instance Show Label where
   show Lponens = "(→)"
   show Labs = "(λ)"
@@ -107,9 +47,11 @@ type Depth = Int
 type TermDiagram = (Exp,Depth,Type)
 
 
+-- | Gets a type derivation diagram of the term.
+-- It fails if the term is non-typeable.
 gentzendiagram :: Exp -> Maybe (ProofTree String String)
 gentzendiagram l = do
-  (_, d) <- typeinfer' variables 0 emptyctx l (Tvar 0)
+  (_, d)   <- typeinfer' variables 0 emptyctx l (Tvar 0)
   maintype <- typeinfer variables emptyctx l (Tvar 0) <*> pure (Tvar 0)
   let (normtemplate, _) = normalizeTemplate Map.empty 0 maintype
   return (bimap (showTermDiagram normtemplate) show d)
@@ -124,10 +66,10 @@ gentzendiagram l = do
 -- be unifiable with this given type.
 -- Generalized version.
 typeinfer' :: [Variable] -- ^ List of fresh variables
-          -> Depth      -- ^ Lambda abstraction depth
-          -> Context    -- ^ Type context
-          -> Exp        -- ^ Lambda expression whose type has to be inferred
-          -> Type       -- ^ Constraint
+          -> Depth       -- ^ Lambda abstraction depth
+          -> Context     -- ^ Type context
+          -> Exp         -- ^ Lambda expression whose type has to be inferred
+          -> Type        -- ^ Constraint
           -> Maybe (Substitution, ProofTree TermDiagram Label)
           
 typeinfer' []  _ _ _ _ = Nothing
